037f3cbfe796db3016434e16e89ed2a10272907a
mgc <chagel@gmail.com>
2026-05-27 17:01:30 -0700

    test: fake pi binary for resolve_bin spec so CI doesn't need pi installed

--- spec/pi_agent_spec.rb
+++ spec/pi_agent_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "tmpdir"
+
 RSpec.describe PiAgent do
   it "has a version number" do
     expect(PiAgent::VERSION).to match(/\A\d+\.\d+\.\d+/)
@@ -11,12 +13,28 @@ RSpec.describe PiAgent do
 
   describe PiAgent::Client do
     it "resolves the pi binary on PATH" do
-      expect(PiAgent::Client.resolve_bin).to match(%r{/pi$})
+      Dir.mktmpdir do |dir|
+        fake = File.join(dir, "pi")
+        File.write(fake, "#!/bin/sh\n")
+        File.chmod(0o755, fake)
+
+        with_path(dir) do
+          expect(PiAgent::Client.resolve_bin).to eq(fake)
+        end
+      end
     end
 
     it "raises BinaryNotFoundError when the binary is missing" do
       expect { PiAgent::Client.resolve_bin("definitely-not-pi-xyz") }
         .to raise_error(PiAgent::BinaryNotFoundError, /Could not find/)
+    end
+
+    def with_path(dir)
+      original = ENV.fetch("PATH", nil)
+      ENV["PATH"] = "#{dir}#{File::PATH_SEPARATOR}#{original}"
+      yield
+    ensure
+      ENV["PATH"] = original
     end
   end
 end
